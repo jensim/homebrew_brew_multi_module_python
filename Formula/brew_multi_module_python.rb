eca49a7befbf1979f309065942876216a9c59692
Jens Brimfors <jens.brimfors@nordnet.se>
2020-09-05 14:39:49 +0200

    Initial commit

---
+++ Formula/brew_multi_module_python.rb
@@ -0,0 +1,12 @@
+class BrewMultiModulePython < Formula
+  desc 'Demo project for delivering multimodule python projects with brew and git'
+  homepage 'https://github.com/jensim/brew_multi_module_python'
+  head 'git@github.com:jensim/brew_multi_module_python.git', :using => :git
+  #url 'git@github.com:jensim/brew_multi_module_python.git', :using => :git, :tag => ''
+  #version ''
+
+  def install
+    bin.install 'runner.sh' => 'brew_multi_module_python'
+    bin.install 'py'
+  end
+end
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # homebrew_brew_multi_module_python
 
 [Code](https://github.com/jensim/brew_multi_module_python)
+
+```
+brew tap demo_jensim/brew_multi_module_python git@github.com:jensim/homebrew_brew_multi_module_python.git
+# brew update
+brew install --HEAD brew_multi_module_python
+```
