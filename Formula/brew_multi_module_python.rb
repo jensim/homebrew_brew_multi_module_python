10a8df555dfcd91ef6402ab4a01aa0f41f5064b9
Jens Brimfors <jens.brimfors@nordnet.se>
2020-09-05 14:52:54 +0200

    Add tag 0.1.0 ref

--- Formula/brew_multi_module_python.rb
+++ Formula/brew_multi_module_python.rb
@@ -2,8 +2,8 @@ class BrewMultiModulePython < Formula
   desc 'Demo project for delivering multimodule python projects with brew and git'
   homepage 'https://github.com/jensim/brew_multi_module_python'
   head 'git@github.com:jensim/brew_multi_module_python.git', :using => :git
-  #url 'git@github.com:jensim/brew_multi_module_python.git', :using => :git, :tag => ''
-  #version ''
+  url 'git@github.com:jensim/brew_multi_module_python.git', :using => :git, :tag => '0.1.0'
+  version '0.1.0'
 
   def install
     bin.install '__main__.py' => 'brew_multi_module_python'
